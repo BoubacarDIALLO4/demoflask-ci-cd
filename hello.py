a77aa9e8facc6e4666867283f1375861d76b343d
Vagrant Default User <vagrant@control>
2019-10-21 10:21:56 +0000

    modifier app

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask
-app = Flask(__name__)
+APP = Flask(__name__)
 
 
 @app.route('/')
@@ -7,4 +7,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    APP.run(host="0.0.0.0")
