ed23b3bd6bceeaf39936265fbe00309c8e9aa89b
Vagrant Default User <vagrant@control>
2019-10-21 10:39:04 +0000

    trois commit

--- hello.py
+++ hello.py
@@ -4,7 +4,8 @@ APP = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """function"""
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    APP.run(host="0.0.0.0")
+    APP.route(host="0.0.0.0")
