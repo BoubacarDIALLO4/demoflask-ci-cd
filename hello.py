f0398eb4aa9b1a059a358e8357a512265df7f492
Vagrant Default User <vagrant@control>
2019-10-21 10:45:59 +0000

    new commit

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+"""ghhhhjj"""
 from flask import Flask
 APP = Flask(__name__)
 
@@ -8,4 +9,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    APP.route(host="0.0.0.0")
+    APP.run(host="0.0.0.0")
